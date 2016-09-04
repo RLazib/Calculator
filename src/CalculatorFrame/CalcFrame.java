17bcd1571d0dc81a282d9bfc910db0f82d73de38
RLazib <ryan.lazib@gmail.com>
2016-09-03 18:38:12 -0700

    Bugfixes, added test package

--- src/CalculatorFrame/CalcFrame.java
+++ src/CalculatorFrame/CalcFrame.java
@@ -544,7 +544,6 @@ public class CalcFrame extends javax.swing.JFrame {
             operandJustSet = false;
             decimalUsed = false;
         }
-        
     }//GEN-LAST:event_equalsButtonActionPerformed
 
     /**
